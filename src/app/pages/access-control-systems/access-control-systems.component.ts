0916836fee4cbc37e0dd74d849569ac0ccc55a0e
Steeve Gaudreault-Lambert <steeveglambert@gmail.com>
2024-02-03 17:23:05 +0000

    add formfield select

--- src/app/pages/access-control-systems/access-control-systems.component.ts
+++ src/app/pages/access-control-systems/access-control-systems.component.ts
@@ -11,8 +11,11 @@ import {
   DynamicFormComponent,
   FormFieldInput,
   FormFieldArray,
+  FormFieldSelectOptionGroup,
+  FormFieldSelect,
 } from '../../../../lib/angular-dynamic-form/projects/dynamic-form/src/public-api';
 import { __values } from 'tslib';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-access-control-systems',
@@ -37,49 +40,81 @@ export class AccessControlSystemsComponent {
     AccessControlDeviceIds: [],
   };
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private apiService: ApiService) {}
 
   openDialogCreate() {
-    const dialogRef = this.dialog
-      .open(DynamicDialogComponent<AccessControlSystem>, {
-        width: '90%',
-        data: {
-          title: `Create a new ${this.pageTitle}`,
-          button: 'Create',
-          formFields: [
-            new FormFieldInput<string>({
-              name: 'name',
-              label: 'Name',
-              hint: '',
-              icon: '',
-              placeholder: '',
-              required: this.isRequired(this.accessControlSystem.Name),
-              type: 'text',
-              value: this.accessControlSystem.Name,
-            }),
-            new FormFieldArray({
-              name: 'accesssystemdevice',
-              label: 'Access System Device',
-              formField: new FormFieldInput<string>({
-                name: 'Subinput',
-                label: 'Sub Input',
-                value: '',
-              }),
-            }),
-          ],
+    let accessControlDeviceOptionGroups: FormFieldSelectOptionGroup[] = [];
+
+    this.apiService.accesControleDeviceService
+      .apiV1AccessControlDeviceGet()
+      .subscribe({
+        next: (value) => {
+          value.forEach((accessControlDevice) => {
+            const groupIndex = accessControlDeviceOptionGroups.findIndex(
+              (optionGroup) => optionGroup.name === accessControlDevice.Type
+            );
+            if (groupIndex > 0) {
+              accessControlDeviceOptionGroups[groupIndex].options.push({
+                value: accessControlDevice.Id as string,
+                viewValue: accessControlDevice.Name,
+              });
+            } else {
+              accessControlDeviceOptionGroups.push({
+                name: accessControlDevice.Type as string,
+                options: [
+                  {
+                    value: accessControlDevice.Id as string,
+                    viewValue: accessControlDevice.Name,
+                  },
+                ],
+              });
+            }
+          });
         },
-      })
-      .componentInstance.formFieldValueChange.subscribe({
-        next: (value: any) => console.debug(value),
-        error: (err: any) => console.error(err),
-        complete: () => console.log('Value change complete.'),
+        error: (err) => console.error(err),
+        complete: () => {
+          this.dialog
+            .open(DynamicDialogComponent<AccessControlSystem>, {
+              width: '90%',
+              data: {
+                title: `Create a new ${this.pageTitle}`,
+                button: 'Create',
+                formFields: [
+                  new FormFieldInput<string>({
+                    name: 'name',
+                    label: 'Name',
+                    hint: '',
+                    icon: '',
+                    placeholder: '',
+                    required: this.isRequired(this.accessControlSystem.Name),
+                    type: 'text',
+                    value: this.accessControlSystem.Name,
+                  }),
+                  new FormFieldArray({
+                    name: 'accesssystemdevice',
+                    label: 'Access System Device',
+                    formField: new FormFieldSelect({
+                      name: 'AccesControleDevice',
+                      label: 'Acces Controle Device',
+                      OptionGroup: accessControlDeviceOptionGroups
+                    }),
+                  }),
+                ],
+              },
+            })
+            .componentInstance.formFieldValueChange.subscribe({
+              next: (value: any) => console.debug(value),
+              error: (err: any) => console.error(err),
+              complete: () => console.log('Value change complete.'),
+            });
+          // .afterClosed()
+          // .subscribe({
+          //   next: (value) => console.debug(value),
+          //   error: (err) => console.error(err),
+          //   complete: () => {},
+          // });}
+        },
       });
-    // .afterClosed()
-    // .subscribe({
-    //   next: (value) => console.debug(value),
-    //   error: (err) => console.error(err),
-    //   complete: () => {},
-    // });
   }
 
   isRequired<T>(property: T | null): boolean {
