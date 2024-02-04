ea4d7f944e6c68947b65df13a3d14410b6e7265e
Steeve Gaudreault-Lambert <steeveglambert@gmail.com>
2024-02-04 01:27:40 +0000

    lib woks

--- src/app/pages/access-control-systems/access-control-systems.component.ts
+++ src/app/pages/access-control-systems/access-control-systems.component.ts
@@ -7,15 +7,16 @@ import { AccessControlSystem } from '@utopiksandcastle/accesscontrol-api-client'
 
 import { MenuButtonComponent } from '../../components/menu-button/menu-button.component';
 import {
-  DynamicDialogComponent,
-  DynamicFormComponent,
   FormFieldInput,
   FormFieldArray,
   FormFieldSelectOptionGroup,
   FormFieldSelect,
+  DynamicDialogComponent,
+  DynamicFormComponent,
 } from '../../../../lib/angular-dynamic-form/projects/dynamic-form/src/public-api';
 import { __values } from 'tslib';
 import { ApiService } from '../../api.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-access-control-systems',
@@ -42,79 +43,87 @@ export class AccessControlSystemsComponent {
 
   constructor(public dialog: MatDialog, private apiService: ApiService) {}
 
-  openDialogCreate() {
-    let accessControlDeviceOptionGroups: FormFieldSelectOptionGroup[] = [];
+  async openDialogCreate() {
+    try {
+      // Retrieve access control devices
+      const accessControlDevices = await firstValueFrom(
+        this.apiService.accesControleDeviceService.apiV1AccessControlDeviceGet()
+      );
 
-    this.apiService.accesControleDeviceService
-      .apiV1AccessControlDeviceGet()
-      .subscribe({
-        next: (value) => {
-          value.forEach((accessControlDevice) => {
-            const groupIndex = accessControlDeviceOptionGroups.findIndex(
-              (optionGroup) => optionGroup.name === accessControlDevice.Type
-            );
-            if (groupIndex > 0) {
-              accessControlDeviceOptionGroups[groupIndex].options.push({
+      if (!accessControlDevices) {
+        // Handle the case where accessControlDevices is undefined
+        console.error('Access control devices are undefined.');
+        return;
+      }
+
+      // Convert access control devices to option groups
+      const accessControlDeviceOptionGroups: FormFieldSelectOptionGroup[] = [];
+
+      accessControlDevices.forEach((accessControlDevice) => {
+        const groupIndex = accessControlDeviceOptionGroups.findIndex(
+          (optionGroup) => optionGroup.name === accessControlDevice.Type
+        );
+
+        if (groupIndex > -1) {
+          accessControlDeviceOptionGroups[groupIndex].options.push({
+            value: accessControlDevice.Id as string,
+            viewValue: accessControlDevice.Name,
+          });
+        } else {
+          accessControlDeviceOptionGroups.push({
+            name: accessControlDevice.Type as string,
+            options: [
+              {
                 value: accessControlDevice.Id as string,
                 viewValue: accessControlDevice.Name,
-              });
-            } else {
-              accessControlDeviceOptionGroups.push({
-                name: accessControlDevice.Type as string,
-                options: [
-                  {
-                    value: accessControlDevice.Id as string,
-                    viewValue: accessControlDevice.Name,
-                  },
-                ],
-              });
-            }
-          });
-        },
-        error: (err) => console.error(err),
-        complete: () => {
-          this.dialog
-            .open(DynamicDialogComponent<AccessControlSystem>, {
-              width: '90%',
-              data: {
-                title: `Create a new ${this.pageTitle}`,
-                button: 'Create',
-                formFields: [
-                  new FormFieldInput<string>({
-                    name: 'name',
-                    label: 'Name',
-                    hint: '',
-                    icon: '',
-                    placeholder: '',
-                    required: this.isRequired(this.accessControlSystem.Name),
-                    type: 'text',
-                    value: this.accessControlSystem.Name,
-                  }),
-                  new FormFieldArray({
-                    name: 'accesssystemdevice',
-                    label: 'Access System Device',
-                    formField: new FormFieldSelect({
-                      name: 'AccesControleDevice',
-                      label: 'Acces Controle Device',
-                      OptionGroup: accessControlDeviceOptionGroups,
-                    }),
-                  }),
-                ],
               },
-            })
-            .componentInstance.formFieldValueChange.subscribe({
-              next: (value: any) => console.debug(value),
-              error: (err: any) => console.error(err),
-              complete: () => console.log('Value change complete.'),
-            });
-          // .afterClosed()
-          // .subscribe({
-          //   next: (value) => console.debug(value),
-          //   error: (err) => console.error(err),
-          //   complete: () => {},
-          // });}
-        },
+            ],
+          });
+        }
       });
+
+      // Open the dialog with the updated option groups
+      const dialogRef = this.dialog.open(
+        DynamicDialogComponent<AccessControlSystem>,
+        {
+          width: '90%',
+          data: {
+            title: `Create a new ${this.pageTitle}`,
+            button: 'Create',
+            formFields: [
+              new FormFieldInput<string>({
+                name: 'name',
+                label: 'Name',
+                hint: '',
+                icon: '',
+                placeholder: '',
+                required: this.isRequired(this.accessControlSystem.Name),
+                type: 'text',
+                value: this.accessControlSystem.Name,
+              }),
+              new FormFieldArray({
+                name: 'accesssystemdevice',
+                label: 'Access System Device',
+                formField: new FormFieldSelect({
+                  name: 'AccesControleDevice',
+                  label: 'Acces Controle Device',
+                  optionGroups: accessControlDeviceOptionGroups,
+                }),
+              }),
+            ],
+          },
+        }
+      );
+
+      // Subscribe to form field value changes
+      dialogRef.componentInstance.formFieldValueChange.subscribe({
+        next: (value: any) => console.debug(value),
+        error: (err: any) => console.error(err),
+        complete: () => console.log('Value change complete.'),
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   isRequired<T>(property: T | null): boolean {
