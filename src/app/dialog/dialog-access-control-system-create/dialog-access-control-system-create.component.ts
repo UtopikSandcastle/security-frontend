b4d8bcf71f3a3e05a5a404c40c5c69a4cc9fe2c1
Steeve Gaudreault-Lambert <steeveglambert@gmail.com>
2024-02-12 02:33:15 +0000

    add dialog for acs-create

---
+++ src/app/dialog/dialog-access-control-system-create/dialog-access-control-system-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-access-control-system-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-access-control-system-create.component.html',
+  styleUrl: './dialog-access-control-system-create.component.scss'
+})
+export class DialogAccessControlSystemCreateComponent {
+
+}
--- src/app/pages/access-control-systems/access-control-systems.component.ts
+++ src/app/pages/access-control-systems/access-control-systems.component.ts
@@ -1,49 +1,130 @@
+import { ApiService } from '../../api.service';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { firstValueFrom, forkJoin } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { AccessControlSystem } from '@utopiksandcastle/accesscontrol-api-client';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MenuButtonComponent } from '../../components/menu-button/menu-button.component';
 import {
-  FormFieldInput,
+  AccessControlDevice,
+  AccessControlDeviceType,
+  AccessControlSystem,
+} from '@utopiksandcastle/accesscontrol-api-client';
+import {
+  AbstractFormField,
+  DynamicDialog,
+  DialogOutput,
+  Action as DialogAction,
   FormFieldArray,
-  FormFieldSelectOptionGroup,
+  FormFieldInput,
   FormFieldSelect,
-  DynamicDialogComponent,
-  DynamicFormComponent,
-} from '../../../../lib/angular-dynamic-form/projects/dynamic-form/src/public-api';
-import { __values } from 'tslib';
-import { ApiService } from '../../api.service';
-import { firstValueFrom } from 'rxjs';
+  FormFieldSelectOptionGroup,
+} from '../../../../lib/angular-components/projects/dynamic-form/src/lib';
+
+export enum Action {
+  Create = 'Create',
+  Update = 'Update',
+}
 
 @Component({
   selector: 'app-access-control-systems',
   standalone: true,
   imports: [
-    DynamicDialogComponent,
-    DynamicFormComponent,
+    CommonModule,
     MatButtonModule,
     MatDialogModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
     MatToolbarModule,
     MenuButtonComponent,
+    MatRippleModule,
   ],
   templateUrl: './access-control-systems.component.html',
   styleUrl: './access-control-systems.component.scss',
 })
 export class AccessControlSystemsComponent {
   pageTitle = 'Access Control System';
+  action = Action.Create;
+
+  AccessControlDeviceType = AccessControlDeviceType;
 
   accessControlSystem: AccessControlSystem = {
     Name: '',
     AccessControlDeviceIds: [],
   };
 
-  constructor(public dialog: MatDialog, private apiService: ApiService) {}
+  dataSource: MatTableDataSource<AccessControlSystem> =
+    new MatTableDataSource();
+  displayedColumns: string[] = ['name', 'accessControlDevices'];
 
-  async openDialogCreate() {
+  constructor(
+    public dialog: MatDialog,
+    iconRegistry: MatIconRegistry,
+    private apiService: ApiService
+  ) {
+    iconRegistry.setDefaultFontSetClass('material-symbols-outlined');
+    this.loadData();
+  }
+
+  loadData() {
+    let accessControlDevices: AccessControlDevice[] = [];
+    let data: AccessControlSystem[] = [];
+
+    const accessControlDeviceObservable =
+      this.apiService.accesControleDeviceService.apiV1AccessControlDeviceGet();
+    const accessControlSystemObservable =
+      this.apiService.accessControlSystemService.apiV1AccessControlSystemGet();
+
+    forkJoin([
+      accessControlDeviceObservable,
+      accessControlSystemObservable,
+    ]).subscribe({
+      next: ([devices, systems]) => {
+        accessControlDevices = devices;
+        data = systems;
+      },
+      error: (err) => console.error(err),
+      complete: () => {
+        this.dataSource.data = data.map((system) => ({
+          ...system,
+          AccessControlDevices: system.AccessControlDeviceIds?.map((id) =>
+            accessControlDevices.find((device) => device.Id === id)
+          ),
+        }));
+      },
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  async openDialogCRUD(
+    accessControlSystem: AccessControlSystem | undefined = undefined
+  ) {
+    if (accessControlSystem) {
+      this.action = Action.Update;
+    } else {
+      this.action = Action.Create;
+    }
+
     try {
       // Retrieve access control devices
       const accessControlDevices = await firstValueFrom(
@@ -82,14 +163,37 @@ export class AccessControlSystemsComponent {
         }
       });
 
+      let formFieldAccessControlDeviceIds: AbstractFormField[] = [];
+      accessControlSystem?.AccessControlDeviceIds?.forEach((id) => {
+        formFieldAccessControlDeviceIds.push(
+          new FormFieldSelect<string>({
+            name: 'AccesControleDevice',
+            title: 'Acces Controle Device',
+            optionGroups: accessControlDeviceOptionGroups,
+            value: id,
+          })
+        );
+      });
+
       // Open the dialog with the updated option groups
-      const dialogRef = this.dialog.open(DynamicDialogComponent, {
+      const dialogRef = this.dialog.open(DynamicDialog, {
         disableClose: true,
         width: '90%',
         data: {
           title: `Create a new ${this.pageTitle}`,
-          button: 'Create',
-          formFields: [
+          buttonActionText: this.action,
+          buttonDelete: this.action == Action.Update,
+          dynFormFields: [
+            new FormFieldInput<string>({
+              name: 'Id',
+              title: 'Id',
+              hint: '',
+              icon: '',
+              placeholder: '',
+              hidden: true,
+              type: 'text',
+              value: accessControlSystem?.Id || '',
+            }),
             new FormFieldInput<string>({
               name: 'Name',
               title: 'Name',
@@ -98,33 +202,81 @@ export class AccessControlSystemsComponent {
               placeholder: '',
               required: this.isRequired(this.accessControlSystem.Name),
               type: 'text',
-              value: this.accessControlSystem.Name,
+              value: accessControlSystem?.Name || '',
             }),
             new FormFieldArray({
               name: 'AccessControlDeviceIds',
               title: 'Access System Devices',
               subtitle: '',
-              uniqueValue: true,
-              formField: new FormFieldSelect<string>({
+              distinct: true,
+              formFieldModel: new FormFieldSelect<string>({
                 name: 'AccesControleDevice',
                 title: 'Acces Controle Device',
                 optionGroups: accessControlDeviceOptionGroups,
               }),
+              formFields: formFieldAccessControlDeviceIds,
             }),
           ],
         },
       });
 
       dialogRef.beforeClosed().subscribe({
-        next: (value) => {
-          console.debug(value);
-          this.apiService.accessControlSystemService
-            .apiV1AccessControlSystemPost(value)
-            .subscribe({
-              next: (value) => console.debug(value),
-              error: (err) => console.error(err),
-              complete: () => {},
-            });
+        next: (dialogOutput: DialogOutput<AccessControlSystem>) => {
+          if (!dialogOutput) {
+            return;
+          }
+
+          switch (dialogOutput.action) {
+            case DialogAction.Close:
+              break;
+
+            case DialogAction.Submit:
+              switch (this.action) {
+                case Action.Create:
+                  this.apiService.accessControlSystemService
+                    .apiV1AccessControlSystemPost(dialogOutput.value)
+                    .subscribe({
+                      next: (value) => console.debug(value),
+                      error: (err) => console.error(err),
+                      complete: () => {
+                        this.loadData();
+                      },
+                    });
+                  break;
+                case Action.Update:
+                  this.apiService.accessControlSystemService
+                    .apiV1AccessControlSystemIdPut(
+                      dialogOutput.value.Id as string,
+                      dialogOutput.value
+                    )
+                    .subscribe({
+                      next: (value) => console.debug(value),
+                      error: (err) => console.error(err),
+                      complete: () => {
+                        this.loadData();
+                      },
+                    });
+                  break;
+              }
+              break;
+
+            case DialogAction.Delete:
+              this.apiService.accessControlSystemService
+                .apiV1AccessControlSystemIdDelete(
+                  dialogOutput.value.Id as string
+                )
+                .subscribe({
+                  next: (value) => console.debug(value),
+                  error: (err) => console.error(err),
+                  complete: () => {
+                    this.loadData();
+                  },
+                });
+              break;
+
+            default:
+              break;
+          }
         },
         error: (err) => console.error(err),
       });
